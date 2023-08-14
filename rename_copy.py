d79b91484adbd248d2e64e1c838af806e2d1335a
sudeshranga7 <140012164+sudeshranga7@users.noreply.github.com>
2023-08-14 14:49:42 +0530

    Update rename_copy.py

--- rename_copy.py
+++ rename_copy.py
@@ -11,18 +11,21 @@ def sanitize_filename(filename):
     sanitized_filename = re.sub(r'[.]+', '.', sanitized_filename)
     return sanitized_filename
 
-def copy_file(source_path, destination_path, source_filename_prefix, source_filename_suffix, copy_previous_day):
+def copy_file(source_path, destination_path, source_filename_prefix, source_filename_suffix):
     try:
         current_time = datetime.datetime.now().time()
         current_date = datetime.datetime.now().date()
         yesterday = current_date - datetime.timedelta(days=1)
 
-        if copy_previous_day and current_time >= datetime.time(0, 0) and current_time <= datetime.time(0, 10):
-            source_file_date = yesterday
-        else:
-            source_file_date = current_date
-
+        source_file_date = current_date
         source_filename_with_date = source_filename_prefix + source_file_date.strftime("%Y%m%d") + source_filename_suffix
+
+        if current_time >= datetime.time(0, 0) and current_time <= datetime.time(0, 10):
+            source_file_path = os.path.join(source_path, source_filename_with_date)
+            if not os.path.exists(source_file_path):
+                source_file_date = yesterday
+                source_filename_with_date = source_filename_prefix + source_file_date.strftime("%Y%m%d") + source_filename_suffix
+
         destination_filename = os.path.basename(source_filename_prefix + source_filename_suffix)
 
         sanitized_destination_filename = sanitize_filename(destination_filename)
@@ -40,16 +43,15 @@ def copy_file(source_path, destination_path, source_filename_prefix, source_file
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 6:
-        print("Usage: python script.py source_path destination_path source_filename_prefix source_filename_suffix copy_previous_day")
+    if len(sys.argv) < 5:
+        print("Usage: python script.py source_path destination_path source_filename_prefix source_filename_suffix")
     else:
         try:
             source_path = sys.argv[1]
             destination_path = sys.argv[2]
             source_filename_prefix = sys.argv[3]
             source_filename_suffix = sys.argv[4]
-            copy_previous_day = sys.argv[5].lower() == 'true'
 
-            copy_file(source_path, destination_path, source_filename_prefix, source_filename_suffix, copy_previous_day)
+            copy_file(source_path, destination_path, source_filename_prefix, source_filename_suffix)
         except Exception as e:
             print(f"An error occurred: {e}")
